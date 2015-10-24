abc0ff7609e0e19c966a6b84071cbeab73d2b22d
taoyuan <torworx@gmail.com>
2015-10-24 23:09:23 +0800

    Initial update

--- examples/qrcode.js
+++ examples/qrcode.js
@@ -1,15 +1,17 @@
+var iconv = require('iconv-lite');
 var escpos = require('../');
 
 var device = new escpos.USB(0x0483, 0x5720);
 //var device = new escpos.Console();
 var printer = new escpos.Printer(device);
 
-var code = 'Hello World!';
+var code = '我思故我在';
 
 printer
   .align('ct')
-  .qrcode(code, 10, 'h')
   .text(code)
+  .qrcode(iconv.encode(code, 'GB2312'), 10, 'h')
+  .text('-------------------------------------')
   .feed(6)
   .flush();
 
