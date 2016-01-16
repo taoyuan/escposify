d13c8d187db937d9e3a2c43c7e7f073d3f3753d0
taoyuan <torworx@gmail.com>
2016-01-16 15:42:18 +0800

    Refactor

--- index.js
+++ index.js
@@ -2,7 +2,8 @@
 
 exports.commands = require('./lib/commands');
 
-exports.Printer = require('./lib/printer');
+exports.Printer = exports.printer = require('./lib/printer');
+
 exports.image = require('./lib/image');
 exports.qrimage = require('./lib/qrimage');
 
